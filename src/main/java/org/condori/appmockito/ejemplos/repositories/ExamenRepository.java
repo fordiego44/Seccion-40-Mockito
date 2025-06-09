aada35971815732e165be1fe1e5192dd2f20ecd1
Diego Armando <fordiego44@gmail.com>
2025-06-09 13:26:00 -0500

    testeando proyecto con mockito

---
+++ src/main/java/org/condori/appmockito/ejemplos/repositories/ExamenRepository.java
@@ -0,0 +1,10 @@
+package org.condori.appmockito.ejemplos.repositories;
+
+import org.condori.appmockito.ejemplos.models.Examen;
+
+import java.util.List;
+
+public interface ExamenRepository {
+    Examen guardar(Examen examen);
+    List<Examen> findAll();
+}
---
+++ src/main/java/org/condori/appmockito/ejemplos/services/ExamenService.java
@@ -0,0 +1,13 @@
+package org.condori.appmockito.ejemplos.services;
+
+import org.condori.appmockito.ejemplos.models.Examen;
+
+import java.util.Optional;
+
+public interface ExamenService {
+    Optional<Examen> findExamenPorNombre(String nombre);
+
+    Examen findExamenPorNombreConPreguntas(String nombre);
+
+    Examen guardar(Examen examen);
+}
